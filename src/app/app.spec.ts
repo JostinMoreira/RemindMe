8a5a33defaa780c3fcfa5fd12cc063e58941bed3
JostinMoreira <jostinmoreira58@gmail.com>
2025-10-28 18:05:02 -0500

    Correción de errores "actions"

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -2,21 +2,18 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App], // ✅ imports para standalone components
-    }).compileComponents();    // ✅ obligatorio
+  let app: App;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({ declarations: [App] });
+    app = TestBed.createComponent(App).componentInstance;
   });
 
   it('debería crear el componente principal', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`debería tener el título 'RemindMe'`, () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual('RemindMe');
   });
 });
