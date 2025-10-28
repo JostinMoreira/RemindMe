657995eba4a4c47dfd09ff659343c2378d8340e0
JostinMoreira <jostinmoreira58@gmail.com>
2025-10-28 16:27:58 -0500

    Agregar CI/CD con Angular + Karma + ChromeHeadless

---
+++ remindme/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+
+    client: {
+      clearContext: false
+    },
+
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/remindme'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
+    },
+
+    reporters: ['progress'],
+
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    restartOnFileChange: false,
+    autoWatch: false
+  });
+};
--- remindme/src/app/app.spec.ts
+++ remindme/src/app/app.spec.ts
@@ -1,25 +1,22 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
-      providers: [provideZonelessChangeDetection()]
-    }).compileComponents();
+      imports: [App], // ✅ imports para standalone components
+    }).compileComponents();    // ✅ obligatorio
   });
 
-  it('should create the app', () => {
+  it('debería crear el componente principal', () => {
     const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it(`debería tener el título 'RemindMe'`, () => {
     const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, remindme');
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('RemindMe');
   });
 });
